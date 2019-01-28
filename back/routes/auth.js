94001140072835540e4290bd6f3009691820d9ee
Claire Lebastard <claire_lebatard@hotmail.com>
2019-01-28 16:55:49 +0100

    authentification video #1

---
+++ back/routes/auth.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const { secretKey } = require('../secretKey.json');
+
+const router = express.Router();
+
+const checkAuthorizationHeader = (req, res, next) => {
+  const authHeader = req.get('authorization');
+  if (!authHeader) {
+    return res.status(401).json({
+      error: 'Vous devez vous identifier'
+    });
+  }
+  const token = authHeader.substr(7);
+  jwt.verify(token, secretKey, (err) => {
+    if (err) {
+      return res.status(401).json({
+        error: 'Token non valide'
+      });
+    }
+  });
+  return next();
+};
+
+router.get('/confidential-info', checkAuthorizationHeader, (req, res) => {
+  res.json([
+    { id: 1, accountKey: '22121975' }
+  ]);
+});
+
+router.post('/signin', (req, res) => {
+  // 1. je recup infos de login
+  const { email, password } = req.body;
+  // 2. je vérifie dans db que user existe
+  // 3. véerifer que passw reçu = passw encrypté dans bdd
+  // 4. générer jwt pour authentifier futures requêtes
+  const userRecord = {
+    id: 1, email: 'john.doe@example.com', avatar: ''
+  };
+  jwt.sign(userRecord, secretKey, (err, token) => {
+    if (err) {
+      return res.status(401).json({
+        error: 'JWT generation failed'
+      });
+    }
+    return res.json({
+      token
+    });
+  });
+});
+
+module.exports = router;
