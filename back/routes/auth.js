02a9da16004b872975b9477655e4951810398fd1
Claire Lebastard <claire_lebatard@hotmail.com>
2019-01-29 10:59:56 +0100

    authentification ok (back uniquement)

--- back/routes/auth.js
+++ back/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
 const bcrypt = require('bcrypt');
 const { secretKey } = require('../secretKey.json');
 const db = require('../db');
@@ -8,23 +9,27 @@ const saltRounds = 10;
 
 const router = express.Router();
 
-const checkAuthorizationHeader = (req, res, next) => {
-  const authHeader = req.get('authorization');
-  if (!authHeader) {
-    return res.status(401).json({
-      error: 'Vous devez vous identifier'
-    });
-  }
-  const token = authHeader.substr(7);
-  jwt.verify(token, secretKey, (err) => {
-    if (err) {
-      return res.status(401).json({
-        error: 'Token non valide'
-      });
-    }
-  });
-  return next();
-};
+// Tout ce code peut être remplacé par express-jwt:
+// const checkAuthorizationHeader = (req, res, next) => {
+//   const authHeader = req.get('authorization');
+//   if (!authHeader) {
+//     return res.status(401).json({
+//       error: 'Vous devez vous identifier'
+//     });
+//   }
+//   const token = authHeader.substr(7);
+//   jwt.verify(token, secretKey, (err) => {
+//     if (err) {
+//       return res.status(401).json({
+//         error: 'Token non valide'
+//       });
+//     }
+//   });
+//   return next();
+// };
+const checkAuthorizationHeader = expressJwt({
+  key: secretKey
+});
 
 router.get('/confidential-info', checkAuthorizationHeader, (req, res) => {
   res.json([
@@ -63,19 +68,36 @@ router.post('/signin', (req, res) => {
   // 1. je recup infos de login
   const { email, password } = req.body;
   // 2. je vérifie dans db que user existe
-  // 3. véerifer que passw reçu = passw encrypté dans bdd
-  // 4. générer jwt pour authentifier futures requêtes
-  const userRecord = {
-    id: 1, email: 'john.doe@example.com', avatar: ''
-  };
-  jwt.sign(userRecord, secretKey, (err, token) => {
-    if (err) {
+  db.query('SELECT * FROM user WHERE email=?', [email], (err, users) => {
+    if (users.length === 0) {
       return res.status(401).json({
-        error: 'JWT generation failed'
+        error: 'Email ou mot de passe incorrect'
       });
     }
-    return res.json({
-      token
+    // 3. véerifer que passw reçu = passw encrypté dans bdd
+    const user = users[0];
+    bcrypt.compare(password, user.password, (error, passwordMatch) => {
+      if (error) {
+        return res.status(500).json({
+          error: err.message
+        });
+      }
+      if (!passwordMatch) {
+        return res.status(401).json({
+          error: 'Email ou mot de passe incorrect'
+        });
+      }
+      // 4. générer jwt pour authentifier futures requêtes
+      jwt.sign(user, secretKey, (err, token) => {
+        if (err) {
+          return res.status(401).json({
+            error: 'JWT generation failed'
+          });
+        }
+        return res.json({
+          token
+        });
+      });
     });
   });
 });
